50968197a7202b820f13afb67a8519a73328cad5
DanteEmery <dkvirtual3@gmail.com>
2022-05-27 02:58:18 -0700

    init HelloSprite

--- HelloSprite.py
+++ HelloSprite.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 
-screen = pygame.display.set_mode((1820, 980), 0, 32)
+screen = pygame.display.set_mode((800, 600), 0, 32)
 
 
 pygame.display.set_caption("Hello Pygame")
